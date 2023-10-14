1ae0ed659f4c2d485b989a562e906202d3c898c0
Nikhil <nikhil.nama@vimeo.com>
2023-10-14 11:11:07 +0530

    EX-1 | Basic println

---
+++ hello_world.go
@@ -0,0 +1,11 @@
+package main
+/** 
+Like other languages the root file will contain the package main
+The execution start from here & from main method
+*/
+
+import "fmt" // GO package to format I/O
+
+func main() {
+	fmt.Println("Hello world")
+}
